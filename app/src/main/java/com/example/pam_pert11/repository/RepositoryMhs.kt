39c32b43f79c35350328f362a5c8b7e9831b59a4
Kakonoomoide <rizkiramada128@gmail.com>
2025-01-08 18:26:55 +0700

    ADD - methode untuk akses firebase

---
+++ app/src/main/java/com/example/pam_pert11/repository/RepositoryMhs.kt
@@ -0,0 +1,16 @@
+package com.example.pam_pert11.repository
+
+import com.example.pam_pert11.model.Mahasiswa
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoryMhs {
+    suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    suspend fun deleteMhs(mahasiswa: Mahasiswa)
+
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
+
+    fun getAllMhs(): Flow<List<Mahasiswa>>
+
+    fun getMhs(nim: String): Flow<Mahasiswa>
+}

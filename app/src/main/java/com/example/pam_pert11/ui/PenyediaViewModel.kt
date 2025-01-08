d4aec0eafac80605e2011bf5c27a45882ba402c2
Kakonoomoide <rizkiramada128@gmail.com>
2025-01-08 18:31:14 +0700

    ADD - Tambahkan PenyediaViewModel dengan Factory untuk HomeViewModel dan test ke MainActivity.kt

---
+++ app/src/main/java/com/example/pam_pert11/ui/PenyediaViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.pam_pert11.ui
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.pam_pert11.MahasiswaApp
+import com.example.pam_pert11.ui.home.viewmodel.HomeViewModel
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        // home view model
+        initializer {
+            HomeViewModel(
+                mahasiswaApp().container.repositoryMhs
+            )
+        }
+    }
+}
+
+fun CreationExtras.mahasiswaApp(): MahasiswaApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApp)

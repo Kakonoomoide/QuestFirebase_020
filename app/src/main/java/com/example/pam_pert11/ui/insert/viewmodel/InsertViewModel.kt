d4421ec5757f1a2ea04e9c95e77d11b6917a4ccf
Kakonoomoide <rizkiramada128@gmail.com>
2025-01-12 22:21:07 +0700

    ADD - data class MahasiswaEvent dan konversi ke model Mahasiswa

---
+++ app/src/main/java/com/example/pam_pert11/ui/insert/viewmodel/InsertViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.pam_pert11.ui.insert.viewmodel
+
+import com.example.pam_pert11.model.Mahasiswa
+
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
+fun MahasiswaEvent.toMhsModel(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    jenisKelamin = jenisKelamin,
+    alamat = alamat,
+    kelas = kelas,
+    angkatan = angkatan
+)

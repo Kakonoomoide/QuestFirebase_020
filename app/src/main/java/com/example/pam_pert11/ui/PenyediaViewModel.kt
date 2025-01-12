6777fba358c63582d71e1142c773a173d9f21a4b
Kakonoomoide <rizkiramada128@gmail.com>
2025-01-12 22:58:38 +0700

    ADD - inisialisasi InsertViewModel pada container aplikasi

--- app/src/main/java/com/example/pam_pert11/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pam_pert11/ui/PenyediaViewModel.kt
@@ -7,12 +7,20 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.pam_pert11.MahasiswaApp
 import com.example.pam_pert11.ui.home.viewmodel.HomeViewModel
+import com.example.pam_pert11.ui.insert.viewmodel.InsertViewModel
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         // home view model
         initializer {
             HomeViewModel(
+                mahasiswaApp().container.repositoryMhs
+            )
+        }
+
+        // insert view model
+        initializer {
+            InsertViewModel(
                 mahasiswaApp().container.repositoryMhs
             )
         }
